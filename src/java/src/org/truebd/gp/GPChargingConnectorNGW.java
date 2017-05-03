1b36472bc0fa8b1e749994e3d046ae7f519e25d9
TslTwl <alamin@truebd.net>
2017-05-03 15:41:00 +0600

    Delete GPChargingConnectorNGW.java

--- src/java/src/org/truebd/gp/GPChargingConnectorNGW.java
+++
@@ -1,8 +0,0 @@
-package src.org.truebd.gp;
-
-import javax.ejb.Local;
-
-@Local
-public interface GPChargingConnectorNGW {
-	public int doChargeNGW(String phone, String amount, String productID, String description, String GPServiceID);
-}
